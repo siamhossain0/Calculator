1615c572ecce77b1037b4c83f9f552a568d45b04
siamhossain0 <mdsiamhosaain0@gmail.com>
2025-05-11 09:44:34 +0600

    initial commit

---
+++ lib/summ_app.dart
@@ -0,0 +1,13 @@
+import 'package:caltulator_sum_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SumApp extends StatelessWidget {
+  const SumApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
